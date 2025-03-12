fc02446fac35e599e52490b2399f0db0b013339e
James Quartly <jquartly@agoge.tech>
2025-03-12 01:09:36 +0000

    Add text panel output to DroneCommand
    Also adds a logging framework to dump debug messages to panels.

--- DroneCommand/DroneCommander.cs
+++ DroneCommand/DroneCommander.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics.Tracing;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using VRage;
 using VRage.Collections;
@@ -17,6 +18,7 @@ using VRage.Game.GUI.TextPanel;
 using VRage.Game.ModAPI.Ingame;
 using VRage.Game.ModAPI.Ingame.Utilities;
 using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Library.Net;
 using VRageMath;
 
 namespace IngameScript
@@ -27,42 +29,149 @@ namespace IngameScript
         static readonly string LAUNCH_ORDER = "GO";
         
         static readonly string STATUS_REPORTING_CHANNEL = "Home carrier drone status";
+        static readonly string DEBUG_PANEL_TAG = "[DebugPanel]";
+        static readonly string DRONE_TAG = "[Drone]";
+        static readonly string MAX_LINES_TAG_START = "[MaxLines:";
+        static readonly int DEFAULT_MAX_LINES = 6;
 
         readonly IMyBroadcastListener m_statusListener;
         readonly Dictionary<string, Drone> m_drones;
+        readonly IMyTextSurface m_myTextSurface;
+        readonly IMyTextSurface m_debugTextSurface;
+        readonly List<IMyTextSurfaceProvider> m_droneStatusSurfaceProviders = new List<IMyTextSurfaceProvider>();
 
         public Program()
         {
             Runtime.UpdateFrequency = UpdateFrequency.Update10;
             m_statusListener = IGC.RegisterBroadcastListener(STATUS_REPORTING_CHANNEL);
             m_drones = new Dictionary<string, Drone>();
+            m_myTextSurface = Me.GetSurface(0);
+            List<IMyTextSurfaceProvider> surfaceProviders = new List<IMyTextSurfaceProvider>();
+            GridTerminalSystem.GetBlocksOfType(surfaceProviders);
+            foreach(var provider in surfaceProviders)
+            {
+                var block = provider as IMyTerminalBlock;
+                if(block != null) 
+                {
+                    if(block.CustomData.Contains(DEBUG_PANEL_TAG))
+                    {
+                        m_debugTextSurface = provider.GetSurface(0);
+                        m_debugTextSurface.ContentType = ContentType.TEXT_AND_IMAGE;
+
+                        int debugLineCount = DEFAULT_MAX_LINES;
+                        var start = block.CustomData.IndexOf(MAX_LINES_TAG_START);
+                        if(start >= 0)
+                        {
+                            start += MAX_LINES_TAG_START.Length;
+                            var end = block.CustomData.IndexOf("]", start);
+                            if(end >= 0)
+                            {
+                                if(int.TryParse(block.CustomData.Substring(start, end - start), out debugLineCount) == false)
+                                {
+                                    debugLineCount = DEFAULT_MAX_LINES;
+                                }
+                            }
+                        }
+                        m_debugPanelLines = new TextPanelBuffer(debugLineCount);
+                    }
+                    else if(block.CustomData.Contains(DRONE_TAG))
+                    {
+                        m_droneStatusSurfaceProviders.Add(provider);
+                    }
+                }
+            }
+        }
+
+        IMyTextSurface FindStatusSurfaceForDrone(string droneName)
+        {
+            foreach(var provider in m_droneStatusSurfaceProviders)
+            {
+                var customData = (provider as IMyTerminalBlock).CustomData;
+                var searchStr = "[" + droneName + ":";
+                var start = customData.IndexOf(searchStr);
+                if(start >= 0)
+                {
+                    start += searchStr.Length;
+                    var end = customData.IndexOf("]", start);
+                    if(end >=0)
+                    {
+                        int surfaceIndex = 0;
+                        if(int.TryParse(customData.Substring(start, end - start), out surfaceIndex) == false)
+                        {
+                            surfaceIndex = 0;
+                        }
+                        return provider.GetSurface(surfaceIndex);
+                    }
+                }
+                else if(customData.Contains("[" + droneName))
+                {
+                    return provider.GetSurface(0);
+                }
+            }
+            Log("No panel found for " + droneName);
+            return null;
+        }
+
+        readonly TextPanelBuffer m_debugPanelLines;
+
+        private void Log(string msg)
+        {
+            if(m_debugTextSurface != null)
+            {
+                m_debugPanelLines.AddLine(msg);
+                m_debugTextSurface.WriteText(m_debugPanelLines.GetLines());
+            }
+            Echo(msg);
+            
         }
 
         public void Main(string argument, UpdateType updateSource)
         {
+            if(argument.StartsWith(RECALL_ORDER))
+            {
+                SendRecallOrder(argument.Substring(RECALL_ORDER.Length + 1));
+                return;
+            }
+            else if(argument.StartsWith(LAUNCH_ORDER))
+            {
+                SendLaunchOrder(argument.Substring(LAUNCH_ORDER.Length + 1));
+                return;
+            }
+            //Process any status updates from drones
             while(m_statusListener.HasPendingMessage)
             {
                 var message = m_statusListener.AcceptMessage().As<String>();
                 HandleStatusUpdate(message);
             }
-            if(argument.StartsWith(RECALL_ORDER))
-            {
-                SendRecallOrder(argument.Substring(RECALL_ORDER.Length + 1));
 
-            }
-            else if(argument.StartsWith(LAUNCH_ORDER))
+            //Update PB Panel
+            if(m_drones.Count > 0)
             {
-                SendLaunchOrder(argument.Substring(LAUNCH_ORDER.Length + 1));
+                m_myTextSurface.WriteText(string.Join("\r\n", m_drones.Values.Select(x => x.Get1LineStatusText())));
+            }
+            else
+            {
+                m_myTextSurface.WriteText("NO DRONES");
+            }
+
+            //Update drone status panels
+            foreach(var drone in m_drones.Values)
+            {
+                if(drone.OutputSurface == null)
+                    continue;
+                drone.OutputSurface.WriteText(drone.GetDetailedPanelText());
             }
         }
 
         void SendLaunchOrder(string droneName)
         {
+            Log("Launching on " + m_drones[droneName].OrdersChannel);
             IGC.SendBroadcastMessage(m_drones[droneName].OrdersChannel, LAUNCH_ORDER);
         }
 
         void SendRecallOrder(string droneName)
         {
+            Log("Recalling on " + m_drones[droneName].OrdersChannel);
             IGC.SendBroadcastMessage(m_drones[droneName].OrdersChannel, RECALL_ORDER);
         }
 
@@ -78,8 +187,8 @@ namespace IngameScript
         void HandleNewDrone(DroneStatus status)
         {
             //TODO Find output surface
-            m_drones[status.DroneName] = new Drone(status, null);
-            Echo("Registered new drone: " + status.DroneName);
+            m_drones[status.DroneName] = new Drone(status, FindStatusSurfaceForDrone(status.DroneName));
+            Log("Registered new drone: " + status.DroneName);
         }
     }
 
@@ -90,10 +199,28 @@ namespace IngameScript
         {
             LastStatus = status;
             OrdersChannel = string.Format(ORDERS_CHANNEL_FMT_STR, status.DroneName);
+            OutputSurface = outputSurface;
         }
         public DroneStatus LastStatus;
         public readonly IMyTextSurface OutputSurface;
         public readonly string OrdersChannel;
+        public string GetDetailedPanelText()
+        {
+            return string.Format("{0}\n{1}\n{2}" +
+                                 "Fuel:  {3:P2}\n" +
+                                 "Power: {4:P2}\n" +
+                                 "Ammo:  {5:P2}\n", 
+                                 LastStatus.DroneName,
+                                 LastStatus.FlightStatus,
+                                 LastStatus.IsDamaged?"DAMAGED\n":"",
+                                 LastStatus.FuelLevel,
+                                 LastStatus.BatteryLevel,
+                                 LastStatus.MagazineLevel);
+        }
+        public string Get1LineStatusText()
+        {
+            return string.Format("{0}{1} - {2}: F{3:P2} P{4:P2} A{5:P2} ", LastStatus.DroneName, LastStatus.IsDamaged?"(D)":"", LastStatus.FlightStatus, LastStatus.FuelLevel, LastStatus.BatteryLevel, LastStatus.MagazineLevel);
+        }
     }
 
     class DroneStatus
@@ -104,22 +231,63 @@ namespace IngameScript
             DroneName = parts[DroneNameIndex];
             FlightStatus = parts[FlightStatusIndex];
             IsDamaged = bool.Parse(parts[IsDamagedIndex]);
-            FuelLevel = float.Parse(parts[FuelLevelIndex]);
-            BatteryLevel = float.Parse(parts[BatteryLevelIndex]);
-            MagazineLevel = float.Parse(parts[MagazineLevelIndex]);
+            FuelLevel = float.Parse(parts[FuelLevelIndex].Replace("%", "")) / 100;
+            BatteryLevel = float.Parse(parts[BatteryLevelIndex].Replace("%", "")) / 100;
+            MagazineLevel = float.Parse(parts[MagazineLevelIndex].Replace("%", "")) / 100;
+            RawMessage = statusStr;
+
         }
-        static readonly int DroneNameIndex = 0;
+        readonly int DroneNameIndex = 0;
         public readonly string DroneName;
-        public readonly int FlightStatusIndex = 1;
+        readonly int FlightStatusIndex = 1;
         public readonly string FlightStatus;
-        public readonly int IsDamagedIndex = 2;
+        readonly int IsDamagedIndex = 2;
         public readonly bool IsDamaged;
-        public readonly int FuelLevelIndex = 3;
+        readonly int FuelLevelIndex = 3;
         public readonly float FuelLevel;
-        public readonly int BatteryLevelIndex = 4;
+        readonly int BatteryLevelIndex = 4;
         public readonly float BatteryLevel;
-        public readonly int MagazineLevelIndex = 5;
+        readonly int MagazineLevelIndex = 5;
         public readonly float MagazineLevel;
+        public readonly string RawMessage;
 
     }
+
+    class TextPanelBuffer
+    {
+        public TextPanelBuffer(int capacity)
+        {
+            m_lines = new string[capacity];
+            m_lastAddedIndex = capacity;
+        }
+        readonly string[] m_lines;
+        private int m_lastAddedIndex;
+        
+        public void AddLine(string line)
+        {
+            m_lastAddedIndex++;
+            if(m_lastAddedIndex >= m_lines.Length)
+            {
+                m_lastAddedIndex = 0;
+            }
+            m_lines[m_lastAddedIndex] = line;
+        }
+
+        public string GetLines()
+        {
+            string outStr = "";
+            for(int lineCount = 0; lineCount < m_lines.Length; lineCount++)
+            {
+                m_lastAddedIndex++;
+                if(m_lastAddedIndex >= m_lines.Length)
+                {
+                    m_lastAddedIndex = 0;
+                }
+                outStr += m_lines[m_lastAddedIndex] + "\n";
+            }
+            return outStr;
+        }
+
+    }
+
 }
